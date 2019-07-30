5481faef35427fe2304cb7095a17594ac903ddc8
myname21-meet <myname21@meet.mit.edu>
2019-07-30 10:26:30 +0300

    starting flask

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask,render_template
 app = Flask(__name__)
-
 @app.route('/')
-def home_page():
-    return "Hello World"
+def home_page(day=False ,foods= ["Ice Cream","Watermelon","Apple"]):
+    return render_template("index.html",day=day,foods=foods)
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(debug =True)
